a769f6279743313fc9a42bb6a225b51d5c49c40f
Anmol Mann <mann.anmol15@gmail.com>
2016-12-28 16:53:46 +0530

    Update Solution.java

--- Solution.java
+++ Solution.java
@@ -13,35 +13,32 @@ public class Solution {
         this.population = population;
         this.load = population/clinics;
     }
-    
-	public static void main(String[] args){
+    public static void main(String[] args) {
+	// Read input from stdin and provide input before running if required
+	Scanner sc = new Scanner(System.in);
+	int n = sc.nextInt();  // cities
+	int b = sc.nextInt();  // clinics
 		
-		// Read input from stdin and provide input before running if required
-		Scanner sc = new Scanner(System.in);
-		int n = sc.nextInt();  // cities
-		int b = sc.nextInt();  // clinics
+	PriorityQueue<Solution> q = new PriorityQueue<Solution>(new Comparator<Solution>() {
+	    public int compare(Solution s1, Solution s2) {
+		if(s1.load < s2.load) return 1;
+		else if(s1.load > s2.load) return -1;
+		return 0;
+	    }
+	});
 		
-		PriorityQueue<Solution> q = new PriorityQueue<Solution>(new Comparator<Solution>() {
-			public int compare(Solution s1, Solution s2) {
-				if(s1.load < s2.load) return 1;
-				else if(s1.load > s2.load) return -1;
-				return 0;
-			}
-		});
-		
-		for (int i = 0; i < n; i++) {
-		    q.add(new Solution(1, sc.nextInt()));
-		}
-		
-		for(int i = 0; i < b - n; i++) {
-			Solution temp = q.poll();
-			temp.clinics++;
-			temp.load = temp.population / temp.clinics;
-			if(temp.population % temp.clinics != 0) ++temp.load;
-			q.add(temp);
-            //System.out.println(temp.load);
-		}
-		System.out.println(q.peek().load);
-		sc.close();
+	for (int i = 0; i < n; i++) {
+	    q.add(new Solution(1, sc.nextInt()));
 	}
+		
+	for(int i = 0; i < b - n; i++) {
+	    Solution temp = q.poll();
+	    temp.clinics++;
+	    temp.load = temp.population / temp.clinics;
+	    if(temp.population % temp.clinics != 0) ++temp.load;
+	    q.add(temp);
+	}
+	System.out.println(q.peek().load);
+	sc.close();
+    }
 }
